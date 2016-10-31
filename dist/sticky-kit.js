f23109413809f827d7f4db8327191890828a5098
sp0ng <blueseaguy@hotmail.com>
2016-10-31 13:39:53 +0100

    Scroll position check when scrolling back to top
    Changed the check to detach the element to smaller or equal than top. If the sticky element is at the top of the page, the position is zero.

--- dist/sticky-kit.js
+++ dist/sticky-kit.js
@@ -156,7 +156,7 @@
               }).trigger("sticky_kit:unbottom");
             }
           }
-          if (scroll < top) {
+          if (scroll <= top) {
             fixed = false;
             offset = offset_top;
             if (manual_spacer == null) {
